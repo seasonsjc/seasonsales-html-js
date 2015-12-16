a31f7e1a6d84f2aeaf286f29a9541cfcdde63f52
ERCARVAL <ercarval@gmail.com>
2015-12-16 17:48:51 -0200

    catalog js

---
+++ src/web/js/app/catalog.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var App = App || {};
+
+App.CatalogUi = (function(){
+
+
+    function CatalogUi () {
+    }
+
+
+
+    CatalogUi.prototype.show = function(){
+
+        var catalogId = '#catalog';
+
+        
+    }
+
+    return CatalogUi;
+})();
+
+
+/**
+ *
+ */
+App.Catalog = (function(){
+
+    var FIELDS = ['name','product', 'availableUntil', 'price'];
+
+    function Catalog (options) {
+        for (var i = 0; i < fields.length; i++) {
+            this[FIELDS[i]] = options[FIELDS[i]] || undefined;
+        }
+    }
+
+    Catalog.prototype.all = function(){
+
+    }
+
+    return Catalog;
+})();
+
+
+/**
+ *
+ */
+App.CatalogService = (function(){
+
+    return {
+
+        list : function (options) {
+
+        },
+
+        add : function ( catalog ) {
+
+        },
+
+
+
+
+    };
+})();
