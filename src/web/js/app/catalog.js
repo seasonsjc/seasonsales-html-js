e0f17b5f76ac849b365fd6a8b8abcb1520a2bc30
ercarval <ercarval@gmail.com>
2015-12-21 19:39:23 -0200

    aula 17/12

--- src/web/js/app/catalog.js
+++ src/web/js/app/catalog.js
@@ -13,30 +13,36 @@ App.CatalogUi = (function(){
 
         var catalog = new App.Catalog();
 
-        catalog.all ( function (products) {
-            $(catalogId).append (
-              '<div class="plan">'
-            + '    <div class="title">'
-            + '        FREE PLAN'
-            + '        <div class="price"><span class="currency">$</span>0<span class="period">/ MO</span></div>'
-            + '        <a class="btn btn-clear btn-small" href="#">AND GET FREE MONTH</a>'
-            + '    </div>'
-            + ''
-            + '    <div class="description">'
-            + '        <div class="description-box">'
-            + '            <span class="fui-cmd"></span> 10,000 messages'
-            + '        </div>'
-            + '        <div class="description-box">'
-            + '            <span class="fui-mic"></span> <b>unlimited</b> data'
-            + '        </div>'
-            + '        <div class="description-box">'
-            + '            <span class="fui-user"></span> <b>unlimited</b> users'
-            + '        </div>'
-            + '        <div class="description-box">'
-            + '            <span class="fui-windows"></span> first 10 day free'
-            + '        </div>'
-            + '    </div>'
-            + '</div>');
+        catalog.all ( function (data) {
+
+            for (var i = 0; i < data.products.length; i++) {
+
+                $(catalogId).append (
+                  '<div class="plan">'
+                + '    <div class="title">'
+                + '        FREE PLAN'
+                + '        <div class="price"><span class="currency">$</span>0<span class="period">/ MO</span></div>'
+                + '        <a class="btn btn-clear btn-small" href="#">AND GET FREE MONTH</a>'
+                + '    </div>'
+                + ''
+                + '    <div class="description">'
+                + '        <div class="description-box">'
+                + '            <span class="fui-cmd"></span> 10,000 messages'
+                + '        </div>'
+                + '        <div class="description-box">'
+                + '            <span class="fui-mic"></span> <b>unlimited</b> data'
+                + '        </div>'
+                + '        <div class="description-box">'
+                + '            <span class="fui-user"></span> <b>unlimited</b> users'
+                + '        </div>'
+                + '        <div class="description-box">'
+                + '            <span class="fui-windows"></span> first 10 day free'
+                + '        </div>'
+                + '    </div>'
+                + '</div>');
+            }
+
+
 
 
 
@@ -65,7 +71,7 @@ App.Catalog = (function() {
     }
 
     Catalog.prototype.all = function(callback){
-        service.list( callback ) ;
+         App.CatalogService.list( callback ) ;
     }
 
     return Catalog;
